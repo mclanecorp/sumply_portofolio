1a1215848c1618f9d2b5f8393a98631d4ec34932
mclanecorp <darkmienworld@gmail.com>
2023-03-10 11:04:41 +0100

    javascript a revoir--responsive-mode dark/light fonctionnelle

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const dark_profile = document.getElementById("dark_profile");
 const light_profile = document.getElementById("light_profile");
 const download_cv = document.getElementById("download_cv");
 const contact_me = document.getElementById("contact_me");
+const tabs_button = document.getElementById("tabs_button");
+const portfolio_tabs = document.getElementById("portfolio_tabs");
+const profile_facts = document.getElementById("profile_facts");
+const skills_tabs = document.getElementById("skills_tabs");
 
 btn_change_theme.addEventListener("click", () => {
   console.log("ok");
@@ -19,6 +23,11 @@ btn_change_theme.addEventListener("click", () => {
     download_cv.style.backgroundColor = "#FFE071";
     contact_me.style.backgroundColor = "#171F26";
     contact_me.style.color = "#A3ABB2";
+    tabs_button.style.backgroundColor = "#171F26";
+    portfolio_tabs.style.color = "#A3ABB2";
+    portfolio_tabs.style.backgroundColor = "#0C151D";
+    profile_facts.style.color = "#A3ABB2";
+    skills_tabs.style.color = "#A3ABB2";
   } else {
     body.style.backgroundColor = "#E5E5E5";
     body.style.color = "#3D3D3D";
@@ -29,5 +38,9 @@ btn_change_theme.addEventListener("click", () => {
     download_cv.style.backgroundColor = "#FBD144";
     contact_me.style.backgroundColor = "#fff";
     contact_me.style.color = "#3D3D3D";
+    tabs_button.style.backgroundColor = "#fff";
+    portfolio_tabs.style.backgroundColor = "#D7D7D7";
+    portfolio_tabs.style.color = "#3D3D3D";
+    skills_tabs.style.color = "#3D3D3D";
   }
 });
